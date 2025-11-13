5a20c0014494b99839023cf1d89fb0df7243e9dd
Pham Sy Phu <phamsyphu0912@gmail.com>
2025-11-13 10:46:02 +0700

    Đã có thể thêm lịch

--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.reminder
 
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TimePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,6 +17,26 @@ import com.google.gson.reflect.TypeToken
 import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
+
+    private fun loadData() {
+        val sharedPref = getSharedPreferences("reminder_prefs", Context.MODE_PRIVATE)
+        val json = sharedPref.getString("reminder_list", null)
+        if (json != null) {
+            val type = object : TypeToken<MutableList<ReminderItem>>() {}.type
+            val loadedList: MutableList<ReminderItem> = gson.fromJson(json, type)
+            reminders.addAll(loadedList)
+        }
+    }
+
+    private fun saveData() {
+        val sharedPref = getSharedPreferences("reminder_prefs", Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        val json = gson.toJson(reminders)
+        editor.putString("reminder_list", json)
+        editor.apply()
+    }
+
+
     private lateinit var adapter: ReminderAdapter
     private val reminders = mutableListOf<ReminderItem>()
     private val gson = Gson()
@@ -23,9 +47,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val reminderView: RecyclerView = findViewById<RecyclerView>(R.id.ReminderSchedule)
         val addButton: Button = findViewById<Button>(R.id.btnAdd)
-
-        addButton.setOnClickListener {
-        }
         loadData()
         adapter = ReminderAdapter(
             reminders,
@@ -41,30 +62,37 @@ class MainActivity : AppCompatActivity() {
         )
         reminderView.adapter = adapter
         reminderView.layoutManager = LinearLayoutManager(this)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+        addButton.setOnClickListener {
+            val dialogView = LayoutInflater.from(this).inflate(R.layout.itemreminder_dialog, null)
+            val timePicker: TimePicker = dialogView.findViewById<TimePicker>(R.id.timePicker)
+            timePicker.setIs24HourView(true)
+            val task: EditText = dialogView.findViewById<EditText>(R.id.editTask)
+            val saveButton: Button = dialogView.findViewById<Button>(R.id.btnSave)
+            val cancelButton: Button = dialogView.findViewById<Button>(R.id.btnCancel)
 
-    private fun loadData()
-    {
-        val sharedPref = getSharedPreferences("reminder_prefs", Context.MODE_PRIVATE)
-        val json = sharedPref.getString("reminder_list", null)
-        if (json != null) {
-            val type = object : TypeToken<MutableList<ReminderItem>>() {}.type
-            val loadedList: MutableList<ReminderItem> = gson.fromJson(json, type)
-            reminders.addAll(loadedList)
-        }
-    }
+            val dialog = AlertDialog.Builder(this)
+                .setTitle("Thêm lịch")
+                .setView(dialogView)
+                .create()
 
-    private fun saveData()
-    {
-        val sharedPref = getSharedPreferences("reminder_prefs", Context.MODE_PRIVATE)
-        val editor = sharedPref.edit()
-        val json = gson.toJson(reminders)
-        editor.putString("reminder_list", json)
-        editor.apply()
+            saveButton.setOnClickListener {
+                val hour = timePicker.hour
+                val minute = timePicker.minute
+                val taskText = task.text.toString()
+                val timestr = String.format("%02d:%02d", hour, minute)
+                reminders.add(ReminderItem(timestr, taskText, true)) //hêm vào list
+                adapter.notifyItemInserted(reminders.size - 1)//báo adapter là có thay đổi
+                saveData() //lưu vào sharedprefs
+                dialog.dismiss() //tắt hộp thoại
+            }
+            cancelButton.setOnClickListener { dialog.dismiss() }
+            dialog.show()
+        }
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
+        }
+
     }
-}
